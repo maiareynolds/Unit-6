fd0fe3c8ac66b85e77961fd16d5c26165b055f9a
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-10 08:27:27 -0400

    Updated from Brython Server: 5/10/2018 8:27:26 AM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,13 @@
+#Maia Reynolds
+#5/10/18
+#howManyWords.py - prints number of words for each number of letters
 
+file=open("engmix.txt")
+
+lengthlist=[]
+for line in file:
+    lengthlist.append(len(line))
+num=1
+while num<=22:
+    print("There are",lengthlist.count(num),num,"letter words")
+    num+=1
