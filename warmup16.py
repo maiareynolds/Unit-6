d463802bc042a5e929680af4460322140b85a4dc
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-11 08:36:14 -0400

    Updated from Brython Server: 5/11/2018 8:36:14 AM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,13 @@
+#Maia Reynolds
+#5/11/18
+#warmup16.py - print out words beginning with first initial and end with last initial
 
+file=open("engmix.txt")
+numWords=0
+for line in file:
+    line=line.strip()
+    if len(line)>1:
+        if line[0] is "m" and line[-1] is "r":
+            print(line)
+            numWords+=1
+print("There are",numWords,"words that start with your first initial and end with your last")
