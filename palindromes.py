62f7ae9bb31c97044812895d7bc1754e28aec384
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-14 08:48:31 -0400

    Updated from Brython Server: 5/14/2018 8:48:31 AM

--- palindromes.py
+++ palindromes.py
@@ -1,13 +1,21 @@
 #Maia Reynolds
 #5/11/18
-#palindromes.py - prints plandromes
+#palindromes.py - prints palindromes
 
 file=open("engmix.txt")
 
+part1=0
+part2=0
 for line in file:
+    line.strip()
+    line=[line]
     if len(line)%2==0:
-        if line[0:((len(line)/2)]==line[((len(line))/2)+1):]:
+        part1=line[0][:((len(line))/2)+1]
+        part2=line[0][(((len(line))/2)+1):]
+        if part1==part2.reverse():
             print(line)
     else:
-        if line[:((len(line)-1)/2)]==line[((len(line)+1)/2):]:
+        part1=line[:((len(line)-1)/2)]
+        part2=line[((len(line)+1)/2):]
+        if part1==part2.reverse():
             print(line)
