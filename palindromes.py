4b580ac114422e1cf52934fcad6650fd9b307b5a
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-16 08:39:27 -0400

    Updated from Brython Server: 5/16/2018 8:39:26 AM

--- palindromes.py
+++ palindromes.py
@@ -4,18 +4,11 @@
 
 file=open("engmix.txt")
 
-part1=0
-part2=0
 for line in file:
     line.strip()
-    line=[line]
-    if len(line)%2==0:
-        part1=line[0][:((len(line))/2)+1]
-        part2=line[0][(((len(line))/2)+1):]
-        if part1==part2.reverse():
-            print(line)
-    else:
-        part1=line[:((len(line)-1)/2)]
-        part2=line[((len(line)+1)/2):]
-        if part1==part2.reverse():
-            print(line)
+    reverse=""
+    for ch in line:
+        reverse=ch+reverse
+    reverse.reverse()
+    if reverse==line:
+        print(line)
