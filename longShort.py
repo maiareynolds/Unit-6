567f42d7eebe829fd61c70efb591e57495495ad6
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-17 08:43:37 -0400

    Updated from Brython Server: 5/17/2018 8:43:36 AM

--- longShort.py
+++ longShort.py
@@ -1,1 +1,24 @@
-g
+#Maia Reynolds
+#5/17/18
+#longShort.py - prints longest and shortest word for each letter in alphabet
+
+file=open("engmix.txt")
+
+alphabet="abcdefghijklmnopqrstuvwxyz"
+
+
+for letter in alphabet:
+    dictionary=[]
+    for line in file:
+        line.split()
+        if line[0]=letter:
+            dictionary.append(line)
+            maxi=0
+            for line in dictionary:
+                if len(line)>=len(maxi):
+                    maxi=line
+            print(maxi)
+            for line in dictionary:
+                if len(line)<=len(maxi):
+                    maxi=line
+            print(maxi)
