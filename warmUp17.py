bdd8d1574ec44c2a9cefe08fc8cb38396f98bcac
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-17 08:26:49 -0400

    Updated from Brython Server: 5/17/2018 8:26:48 AM

--- warmUp17.py
+++ warmUp17.py
@@ -1,1 +1,15 @@
+#Maia Reynolds
+#5/17/18
+#warmup17 - prints all words that contain letters of last name
 
+file=open("engmix.txt")
+
+name=input("Enter your last name: ")
+
+inword=0
+for line in file:
+    for letter in line:
+        if letter in name:
+            inword+=1
+    if len(line)==inword:
+        print(line)
