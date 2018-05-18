a10c7b13a62aac9c167773e8ae7628e18d196ac8
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-18 08:44:34 -0400

    Updated from Brython Server: 5/18/2018 8:44:34 AM

--- practiceTest.py
+++ practiceTest.py
@@ -14,10 +14,25 @@ for line in file:#number of words beginning with r
     if len(line)>=1 and line[0]=="r":
         count+=1
 print(count,"words start with the letter r")
-"""
-num=int(input("Enter a number: "))
+
+num=int(input("Enter a number: "))#print word with number of letters
 words=[]
 for line in file:
     if len(line)==num:
         words.append(line)
 print(words[0])
+
+letter=input("Enter a letter: ")#number of words without letter in them
+times=0
+for line in file:
+    line.split()
+    if len(line)>=1 and letter not in line:
+        times+=1
+print(times,"words do not have the letter",letter,"in them")
+"""
+dictionary=[]
+for line in file:
+    line.split()
+    if len(line)>=1:
+        dictionary.append(line)
+print("The middle word is",dictionary[len(dictionary)/2])
