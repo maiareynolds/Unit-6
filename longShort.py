92bb4164aad190a29791358d3a6f0e4ba45edc0a
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-18 08:48:41 -0400

    Updated from Brython Server: 5/18/2018 8:48:40 AM

--- longShort.py
+++ longShort.py
@@ -6,12 +6,14 @@ file=open("engmix.txt")
 
 alphabet="abcdefghijklmnopqrstuvwxyz"
 
-short=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+short=["a","b","c","d","e","f","g","h","i","j"]
 longest=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
 
 maxi=""
 for line in file:
     line.split()
+    for letter in longest:
+        if longest
     if len(line)>=len(maxi):
         longest[alphabet.index(line[0])]=line
 print(longest)
