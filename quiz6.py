9be02c5321a1ab345c5b47d44b63d44ec01798ca
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-21 08:26:28 -0400

    Updated from Brython Server: 5/21/2018 8:26:28 AM

--- quiz6.py
+++ quiz6.py
@@ -26,11 +26,22 @@ for line in file:
     line.split()
     if len(line)==number+1 and line[0]==letter:
         print(line)
-"""
+
 #4 - finds 8000th word in dictionary file with more than 10 letters
 longwords=[]
 for line in file:#adds words with ten or more letters to longwords list
     line.strip()
     if len(line)>=10:
         longwords.append(line)
-print(longwords[7999])
+print(longwords[7999])#prints 8000th word in list
+"""
+#5 - word in dictionary with greatest number of vowels (a,e,i,o,u)
+vowels=0
+word=""
+for line in file:
+    line.strip()
+    lineVowels=line.count("a")+line.count("e")+line.count("i")+line.count("o")+line.count("u")
+    if len(line)>=1 and lineVowels>vowels:
+        word=line
+        vowels=lineVowels
+print(word)
