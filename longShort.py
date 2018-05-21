f0943a2c33d00edc1bb3411efefc70b85e52ffc5
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-05-21 07:14:21 -0400

    Updated from Brython Server: 5/21/2018 7:14:20 AM

--- longShort.py
+++ longShort.py
@@ -9,10 +9,8 @@ alphabet="abcdefghijklmnopqrstuvwxyz"
 short=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 longest=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-maximum=""
 for line in file:
     line.split()
-    for letter in longest:
-        if line[0]==letter and len(line)>=len(maximum):
-            longest[alphabet.index(letter)]=line
+    if len(line)>=1 and len(line)>=len(longest[alphabet.index(line[0])]):
+        longest[alphabet.index(line[0])]=line
 print(longest)
